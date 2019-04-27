badb7b9ea32cc95c8f8ded5be1fdfcd39300847c
adehad <26027314+adehad@users.noreply.github.com>
2019-04-27 18:57:01 +0100

    updated to match our usage

--- configFiles/StandardConfig_MOVEME.m
+++ configFiles/StandardConfig_MOVEME.m
@@ -1,24 +1,26 @@
 ops.GPU                 = 1; % whether to run this code on an Nvidia GPU (much faster, mexGPUall first)		
-ops.parfor              = 0; % whether to use parfor to accelerate some parts of the algorithm		
+ops.parfor              = 1; % whether to use parfor to accelerate some parts of the algorithm		
 ops.verbose             = 1; % whether to print command line progress		
-ops.showfigures         = 1; % whether to plot figures during optimization		
+ops.showfigures         = 0; % whether to plot figures during optimization		
 		
 ops.datatype            = 'dat';  % binary ('dat', 'bin') or 'openEphys'		
 ops.fbinary             = 'C:\DATA\Spikes\Piroska\piroska_example_short.dat'; % will be created for 'openEphys'		
 ops.fproc               = 'C:\DATA\Spikes\Piroska\temp_wh.dat'; % residual from RAM of preprocessed data		
 ops.root                = 'C:\DATA\Spikes\Piroska'; % 'openEphys' only: where raw files are		
 		
-ops.fs                  = 25000;        % sampling rate		(omit if already in chanMap file)
-ops.NchanTOT            = 32;           % total number of channels (omit if already in chanMap file)
-ops.Nchan               = 32;           % number of active channels (omit if already in chanMap file)
-ops.Nfilt               = 64;           % number of clusters to use (2-4 times more than Nchan, should be a multiple of 32)     		
+% ops.fs                  = 25000;        % sampling rate		(omit if already in chanMap file)
+% ops.NchanTOT            = 32;           % total number of channels (omit if already in chanMap file)
+% ops.Nchan               = 32;           % number of active channels (omit if already in chanMap file)
+ops.Nfilt               = 12;             % number of clusters to use (2-4 times more than Nchan, should be a multiple of 32)     		
 ops.nNeighPC            = 12; % visualization only (Phy): number of channnels to mask the PCs, leave empty to skip (12)		
+			% must be less than number of channels
 ops.nNeigh              = 16; % visualization only (Phy): number of neighboring templates to retain projections of (16)		
-		
+			% Must be less than Nfilt 
+
 % options for channel whitening		
-ops.whitening           = 'full'; % type of whitening (default 'full', for 'noSpikes' set options for spike detection below)		
-ops.nSkipCov            = 1; % compute whitening matrix from every N-th batch (1)		
-ops.whiteningRange      = 32; % how many channels to whiten together (Inf for whole probe whitening, should be fine if Nchan<=32)		
+ops.whitening           = 'full'; 	% type of whitening (default 'full', for 'noSpikes' set options for spike detection below)		
+ops.nSkipCov            = 1; 		% compute whitening matrix from every N-th batch (1)		
+ops.whiteningRange      = 32; 		% how many channels to whiten together (Inf for whole probe whitening, should be fine if Nchan<=32)		
 		
 % define the channel map as a filename (string) or simply an array		
 ops.chanMap             = 'C:\DATA\Spikes\Piroska\chanMap.mat'; % make this file using createChannelMapFile.m		
@@ -26,39 +28,43 @@ ops.criterionNoiseChannels = 0.2; % fraction of "noise" templates allowed to spa
 % ops.chanMap = 1:ops.Nchan; % treated as linear probe if a chanMap file		
 		
 % other options for controlling the model and optimization		
-ops.Nrank               = 3;    % matrix rank of spike template model (3)		
-ops.nfullpasses         = 6;    % number of complete passes through data during optimization (6)		
-ops.maxFR               = 20000;  % maximum number of spikes to extract per batch (20000)		
-ops.fshigh              = 300;   % frequency for high pass filtering		
-% ops.fslow             = 2000;   % frequency for low pass filtering (optional)
-ops.ntbuff              = 64;    % samples of symmetrical buffer for whitening and spike detection		
-ops.scaleproc           = 200;   % int16 scaling of whitened data		
-ops.NT                  = 32*1024+ ops.ntbuff;% this is the batch size (try decreasing if out of memory) 		
-% for GPU should be multiple of 32 + ntbuff		
+ops.Nrank               = 3;    	% matrix rank of spike template model (3)		
+ops.nfullpasses         = 6;    	% number of complete passes through data during optimization (6)		
+ops.maxFR               = 20000;  	% maximum number of spikes to extract per batch (20000)		
+ops.fshigh              = 300;   	% frequency for high pass filtering		
+% ops.fslow             = 2000;   	% frequency for low pass filtering (optional)
+ops.ntbuff              = 64;    	% samples of symmetrical buffer for whitening and spike detection		
+ops.scaleproc           = 200;   	% int16 scaling of whitened data		
+ops.NT                  = 32*1024+ ops.ntbuff; % this is the batch size (try decreasing if out of memory) 		
+			% for GPU should be multiple of 32 + ntbuff		
+
+ops.nt0 = 21; % set samples to take on either side of the detected peak (61)
 		
+	
 % the following options can improve/deteriorate results. 		
 % when multiple values are provided for an option, the first two are beginning and ending anneal values, 		
 % the third is the value used in the final pass. 		
-ops.Th               = [4 10 10];    % threshold for detecting spikes on template-filtered data ([6 12 12])		
+ops.Th               = [4 10 10];   % threshold for detecting spikes on template-filtered data ([6 12 12])		
 ops.lam              = [5 20 20];   % large means amplitudes are forced around the mean ([10 30 30])		
-ops.nannealpasses    = 4;            % should be less than nfullpasses (4)		
-ops.momentum         = 1./[20 400];  % start with high momentum and anneal (1./[20 1000])		
-ops.shuffle_clusters = 1;            % allow merges and splits during optimization (1)		
-ops.mergeT           = .1;           % upper threshold for merging (.1)		
-ops.splitT           = .1;           % lower threshold for splitting (.1)		
+ops.nannealpasses    = 4;           % should be less than nfullpasses (4)		
+ops.momentum         = 1./[20 400]; % start with high momentum and anneal (1./[20 1000])		
+ops.shuffle_clusters = 1;           % allow merges and splits during optimization (1)	
+ops.freqUpdate       = 200;         % after how many batches, should try split and merge (400)	
+ops.mergeT           = 1e-3;        % upper threshold for merging (.1)		
+ops.splitT           = 1e-3;        % lower threshold for splitting (.1)		
 		
 % options for initializing spikes from data		
-ops.initialize      = 'no'; %'fromData' or 'no'		
-ops.spkTh           = -6;      % spike threshold in standard deviations (4)		
-ops.loc_range       = [3  1];  % ranges to detect peaks; plus/minus in time and channel ([3 1])		
-ops.long_range      = [30  6]; % ranges to detect isolated peaks ([30 6])		
-ops.maskMaxChannels = 5;       % how many channels to mask up/down ([5])		
-ops.crit            = .65;     % upper criterion for discarding spike repeates (0.65)		
-ops.nFiltMax        = 10000;   % maximum "unique" spikes to consider (10000)		
+ops.initialize      = 'fromData'; 	%'fromData' or 'no'		
+ops.spkTh           = -5;      		% spike threshold in standard deviations (4)		
+ops.loc_range       = [10  1];  	% ranges to detect peaks; plus/minus in time and channel ([3 1])		
+ops.long_range      = [30  6]; 		% ranges to detect isolated peaks ([30 6])		
+ops.maskMaxChannels = 5;       		% how many channels to mask up/down ([5])		
+ops.crit            = .65;     		% upper criterion for discarding spike repeates (0.65)		
+ops.nFiltMax        = 10e3;   		% maximum "unique" spikes to consider (10000)		
 		
 % load predefined principal components (visualization only (Phy): used for features)		
 dd                  = load('PCspikes2.mat'); % you might want to recompute this from your own data		
-ops.wPCA            = dd.Wi(:,1:7);   % PCs 		
+ops.wPCA            = dd.Wi(:,1:7);   		 % PCs 		
 		
 % options for posthoc merges (under construction)		
 ops.fracse  = 0.1; % binning step along discriminant axis for posthoc merges (in units of sd)		
--- master_file_example_MOVEME.m
+++ master_file_example_MOVEME.m
@@ -1,34 +1,89 @@
-% default options are in parenthesis after the comment
+% Customised for NBits Lab Imperial College London
+% Usage requires configfile, masterfile and binary data (see formatBin_wrapper)
+% files in the same folder
 
 addpath(genpath('D:\CODE\GitHub\KiloSort')) % path to kilosort folder
 addpath(genpath('D:\CODE\GitHub\npy-matlab')) % path to npy-matlab scripts
 
-pathToYourConfigFile = 'D:\CODE\Kilosort\configFiles'; % take from Github folder and put it somewhere else (together with the master_file)
+pathToYourConfigFile = ''; % put masterfile in same folder as config
+                           % ops.root in configfile should be the folder 
+                           % where the masterfile is
 run(fullfile(pathToYourConfigFile, 'StandardConfig_MOVEME.m'))
 
 tic; % start timer
-%
+
 if ops.GPU     
     gpuDevice(1); % initialize GPU (will erase any existing GPU arrays)
 end
 
-if strcmp(ops.datatype , 'openEphys')
-   ops = convertOpenEphysToRawBInary(ops);  % convert data, only for OpenEphys
-end
-%
+% A formatBin_wrapper.m is used for our data, to allow channel padding
+% if strcmp(ops.datatype , 'openEphys')
+%    ops = convertOpenEphysToRawBInary(ops);  % convert data, only for OpenEphys
+% end
+
 [rez, DATA, uproj] = preprocessData(ops); % preprocess data and extract spikes for initialization
 rez                = fitTemplates(rez, DATA, uproj);  % fit templates iteratively
 rez                = fullMPMU(rez, DATA);% extract final spike times (overlapping extraction)
 
-% AutoMerge. rez2Phy will use for clusters the new 5th column of st3 if you run this)
-%     rez = merge_posthoc2(rez);
 
-% save matlab results file
-save(fullfile(ops.root,  'rez.mat'), 'rez', '-v7.3');
+% save matlab results 
+saveOutput(rez,ops,'pre')
 
-% save python results file for Phy
-rezToPhy(rez, ops.root);
+
+%% AUTOMERGES
+% Kilosort's AUTO merges should not be confused with the "best" merges 
+
+% rez = merge_posthoc2(rez);
+
+% saveOutput(rez,ops,'post')
 
 % remove temporary file
-delete(ops.fproc);
-%%
+% delete(ops.fproc);
+
+disp('Done')
+
+% Custom save function
+function saveOutput(rez,ops,preOrPost)
+
+if strcmpi(preOrPost,'pre')
+    runType = 'preAutoMerge';
+    rezName = 'rez.mat';
+elseif strcmpi(preOrPost,'post')
+    runType = 'postAutoMerge';
+    rezName = 'rez_post.mat';
+else
+    error('Must be ''pre'' or ''post'' ')
+end
+
+% save matlab results file
+save(fullfile(ops.root,  rezName), 'rez', '-v7.3');
+
+% save python results file for Phy, in a new folder
+mkdir( [ops.root, runType] ) 
+rezToPhy(rez, [ops.root, filesep, runType, filesep]);
+
+
+% To prevent conflicts that arise due to the .phy folder that is
+% created in the same location as the binary file
+% We use the 'link' method, which prevents taking up actual hard drive space
+if ismac        % Code to run on Mac platform
+    fprintf(['Mac currently not supported. To use Phy: \n', ...
+    'Please move the binary file to the folder \n', ...
+    'that you want to visualise \n'])
+
+elseif isunix    % Code to run on Linux platform
+    command = ['ln',' ',ops.fbinary,' ',runType, filesep, ops.fbinary];
+    [status,cmdout] = system(command);
+    
+elseif ispc      % Code to run on Windows platform
+    command = ['mklink /H',' ', runType, filesep,ops.fbinary,' ',ops.fbinary];
+    [status,cmdout] = system(command);
+    
+else
+    status = -1;
+    cmdout = 'Platform not supported';
+    
+end
+fprintf('Link Status: %i. \n %s',status,cmdout);
+
+end
